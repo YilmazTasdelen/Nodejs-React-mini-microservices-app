ff43ff288491bac8978efb2835f4f10150e55425
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-19 23:52:43 +0300

    .

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -21,7 +24,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+    console.log('Received event', req.body.type);
+    res.status(201).send({});
 });
 
 app.listen(4001, () => {
