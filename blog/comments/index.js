53f3daa92825fc1a8edfb7ef67060615d27f4d95
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-20 22:26:04 +0300

    .

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get('/posts/:id/comments', async (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
@@ -41,6 +41,31 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 app.post('/events', async (req, res) => {
     console.log('Received event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status } = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+    }
+
+
+    await axios.post('http:/localhost:4005/events', {
+        type: 'CommentUpdated',
+        data: {
+            id,
+            status,
+            postId,
+            content
+        }
+    })
+
+
     res.status(201).send({});
 });
 
